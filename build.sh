cb359e741abc018f963fbd89185aa498397f8d51
Szymon Walter <walter.szymon.98@gmail.com>
2017-06-28 18:11:46 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -5,20 +5,24 @@ echo "Creating $2..."
 rm -f $2
 echo "rm -f $2"
 
-cd $1/; zip -9 -r $1.love *
-echo "cd $1/; zip -9 -r $1.love *"
+cd $1/
+zip -9 -r ../$2 *
+echo "cd $1/; zip -9 -r ../$2 *"
 
-cd lib4/; zip -9 -r ../$2 *
-echo "cd lib4/; zip -9 -r ../$2 *"
+cd ../lib4/
+zip -9 -r ../$2 *
+echo "cd ../lib4/; zip -9 -r ../$2 *"
+cd ..
 
-zip -9 -r $2 lib/
-echo "zip -9 -r $2 lib/"
+for lib in cpml iqm love3d; do
+    zip -9 -r $2 lib/$lib
+done
 
 for lib in anim9 autobatch log tick; do
     mv lib/$lib/$lib.lua lib/$lib/init.lua
     echo "mv lib/$lib/$lib.lua lib/$lib/init.lua"
 
-    zip -9 -r $2 lib/$lib/*
+    zip -9 -r $2 lib/$lib
     echo "zip -9 -r $2 lib/$lib/*"
 
     mv lib/$lib/init.lua lib/$lib/$lib.lua
