d6ae9b5daedae50e5f307c771a6bf613f3dc6b63
Szymon Walter <walter.szymon.98@gmail.com>
2017-06-28 19:14:00 +0200

    Reverse echo order

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 
 echo "Creating $2..."
 
-rm -f $2
 echo "rm -f $2"
+rm -f $2
 
+echo "cd $1/; zip -9 -r ../$2 *"
 cd $1/
 zip -9 -r ../$2 *
-echo "cd $1/; zip -9 -r ../$2 *"
 
+echo "cd ../lib4/; zip -9 -r ../$2 *"
 cd ../lib4/
 zip -9 -r ../$2 *
-echo "cd ../lib4/; zip -9 -r ../$2 *"
 cd ..
 
 for lib in cpml iqm love3d; do
@@ -19,14 +19,14 @@ for lib in cpml iqm love3d; do
 done
 
 for lib in anim9 autobatch log tick; do
-    mv lib/$lib/$lib.lua lib/$lib/init.lua
     echo "mv lib/$lib/$lib.lua lib/$lib/init.lua"
+    mv lib/$lib/$lib.lua lib/$lib/init.lua
 
-    zip -9 -r $2 lib/$lib
     echo "zip -9 -r $2 lib/$lib/*"
+    zip -9 -r $2 lib/$lib
 
-    mv lib/$lib/init.lua lib/$lib/$lib.lua
     echo "mv lib/$lib/init.lua lib/$lib/$lib.lua"
+    mv lib/$lib/init.lua lib/$lib/$lib.lua
 done
 
 echo "Done."
