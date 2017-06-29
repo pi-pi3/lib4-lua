ee013a434b8f4284e876db328effd78d59045630
Szymon Walter <walter.szymon.98@gmail.com>
2017-06-29 09:13:34 +0200

    Rewrite build.sh

--- build.sh
+++ build.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 
+dir=$PWD
 echo "Creating $2..."
 
-echo "rm -f $2"
 rm -f $2
 
-echo "cd $1/; zip -9 -r ../$2 *"
+cd lib4/
+zip -9 -r ../$2 main.lua conf.lua
+cd $dir
+
+for f in $(find lib4 -type f); do
+    [ $f != 'lib4/main.lua' ] && zip -9 -r $2 $f
+done
+
 cd $1/
-zip -9 -r ../$2 *
+for f in $(find . -type f -regex '.*\.lua'); do
+    zip -9 -r ../$2 $f
+done
 
-echo "cd ../lib4/; zip -9 -r ../$2 *"
-cd ../lib4/
-zip -9 -r ../$2 *
-cd ..
-
-zip -9 -r $2 assets LICENSE README.md
+for f in src assets LICENSE README.md; do
+    [[ -e ./$f ]] && zip -9 -r ../$2 $f
+done
+cd $dir
 
 for lib in cpml iqm love3d; do
     zip -9 -r $2 lib/$lib
 done
 
 for lib in anim9 autobatch log tick json; do
-    echo "mv lib/$lib/$lib.lua lib/$lib/init.lua"
     mv lib/$lib/$lib.lua lib/$lib/init.lua
-
-    echo "zip -9 -r $2 lib/$lib/*"
     zip -9 -r $2 lib/$lib
-
-    echo "mv lib/$lib/init.lua lib/$lib/$lib.lua"
     mv lib/$lib/init.lua lib/$lib/$lib.lua
 done
 
