079f9b7b189e2baa418ccbabcc3ec121d90dceea
Szymon Walter <walter.szymon.98@gmail.com>
2017-07-02 15:16:16 +0200

    Fix the 'keydown' event

--- lib4/main.lua
+++ lib4/main.lua
@@ -143,7 +143,7 @@ function love.update(dt)
     end
 
     if lib4.keyevents then
-        for _, scancode in ipairs({}) do
+        for _, scancode in ipairs(lib4.scancodes) do
             if love.keyboard.isScancodeDown(scancode) then
                 local key = love.keyboard.getKeyFromScancode(scancode)
                 love.keydown(key, scancode)
