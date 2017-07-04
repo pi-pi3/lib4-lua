c411dc8824cd8ac4db980c19dc6cae1f3f785d4c
Szymon Walter <walter.szymon.98@gmail.com>
2017-07-04 18:54:51 +0200

    Bugfix

--- example/src/game.lua
+++ example/src/game.lua
@@ -3,12 +3,13 @@ local phys = require('lib4/phys')
 
 local game = {}
 
-function game._load()
+function game:_load()
     phys.enable()
+
     game.time = 0
 end
 
-function game._update()
+function game:_update(dt)
     game.time = game.time + dt
     if game.time >= 1 then
         game.time = game.time - 1
