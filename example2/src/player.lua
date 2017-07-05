84195fd1bbdb85d5d54f106cdefd66d855e538b8
Szymon Walter <walter.szymon.98@gmail.com>
2017-07-05 16:26:46 +0200

    Add proper jumping

--- example2/src/player.lua
+++ example2/src/player.lua
@@ -7,20 +7,38 @@ function player:_load()
     self:add(rect(0, 0, 24, 24))
 
     self.speed = 220000
-    self.jump = 610000
-end
-
-function player:_update()
-    print(self.body:getLinearVelocity())
+    self.jump = 61000
+    self.on_ground = false
 end
 
 function player:_keydown(_, key)
+    local mult = 1
+    if not self.on_ground then
+        mult = 0.5
+    end
+
     if key == 'h' then
-        self.body:applyForce(-self.speed, 0)
+        self.body:applyForce(-self.speed*mult, 0)
     elseif key == 'l' then
-        self.body:applyForce(self.speed, 0)
-    elseif key == 'k' then
-        self.body:applyForce(0, -self.jump)
+        self.body:applyForce(self.speed*mult, 0)
+    elseif key == 'k' and self.on_ground then
+        self.body:applyLinearImpulse(0, -self.jump)
+    end
+end
+
+function player:_pre_contact(other, coll)
+    local nx, ny = coll:getNormal()
+    print('pre', nx, ny)
+    if ny > 0.85 then -- approx. 45 deg
+        self.on_ground = true
+    end
+end
+
+function player:_post_contact(other, coll)
+    local nx, ny = coll:getNormal()
+    print('post', nx, ny)
+    if ny == 0 then -- approx. 45 deg
+        self.on_ground = false
     end
 end
 
