5f0bbe9fceaa968075f111485ca68061f9bd8b5f
Szymon Walter <walter.szymon.98@gmail.com>
2017-07-05 17:56:39 +0200

    Hops

--- example2/src/player.lua
+++ example2/src/player.lua
@@ -6,29 +6,31 @@ function player:_load()
     self:set_params()
     self:add(rect(0, 0, 24, 24))
 
-    self.speed = 220000
+    self.speed = 22000
     self.jump = 61000
     self.on_ground = false
 end
 
 function player:_keydown(_, key)
-    local mult = 1
-    if not self.on_ground then
-        mult = 0.5
-    end
-
-    if key == 'h' then
-        self.body:applyForce(-self.speed*mult, 0)
-    elseif key == 'l' then
-        self.body:applyForce(self.speed*mult, 0)
-    elseif key == 'k' and self.on_ground then
-        self.body:applyLinearImpulse(0, -self.jump)
+    if self.on_ground then
+        if key == 'h' then
+            self.body:applyLinearImpulse(-self.speed, -self.jump/4)
+        elseif key == 'l' then
+            self.body:applyLinearImpulse(self.speed, -self.jump/4)
+        elseif key == 'k' then
+            self.body:applyLinearImpulse(0, -self.jump)
+        end
+    else
+        if key == 'h' then
+            self.body:applyForce(-self.speed, 0)
+        elseif key == 'l' then
+            self.body:applyForce(self.speed, 0)
+        end
     end
 end
 
 function player:_pre_contact(other, coll)
     local nx, ny = coll:getNormal()
-    print('pre', nx, ny)
     if ny > 0.85 then -- approx. 45 deg
         self.on_ground = true
     end
@@ -36,7 +38,6 @@ end
 
 function player:_post_contact(other, coll)
     local nx, ny = coll:getNormal()
-    print('post', nx, ny)
     if ny == 0 then -- approx. 45 deg
         self.on_ground = false
     end
