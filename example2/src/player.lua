f38f8d14998d4924b0ef9d12a7d8ba9229201426
Szymon Walter <walter.szymon.98@gmail.com>
2017-07-06 14:00:55 +0200

    Many changes

--- example2/src/player.lua
+++ example2/src/player.lua
@@ -4,36 +4,54 @@ local player = {}
 
 function player:_load()
     self.speed = 22000
-    self.jump = 61000
+    self.jump = 80000
     self.coll_ground = {}
     self.ground = 0
+    self.hop_time = 0.25
+    self.hop = 0.0
 end
 
 function player:on_ground()
     return self.ground > 0
 end
 
+function player:_update(dt)
+    if self.hop > 0.0 then self.hop = self.hop - dt end
+end
+
 function player:_keydown(_, key)
     if self:on_ground() then
         if key == 'h' then
-            self.body:applyLinearImpulse(-self.speed, -self.jump/4)
+            self.body:applyLinearImpulse(-self.speed, -self.jump*0.25)
+            self.hop = self.hop_time
         elseif key == 'l' then
-            self.body:applyLinearImpulse(self.speed, -self.jump/4)
+            self.body:applyLinearImpulse(self.speed, -self.jump*0.25)
+            self.hop = self.hop_time
         elseif key == 'k' then
-            self.body:applyLinearImpulse(0, -self.jump)
+            if love.keyboard.isScancodeDown('h', 'l') then
+                self.body:applyLinearImpulse(0, -self.jump*0.7)
+            else
+                self.body:applyLinearImpulse(0, -self.jump)
+            end
         end
     else
+        local mult = 1
+        local jmult = 0
+        if self.hop > 0.0 then
+            mult = 7.5
+            jmult = 1
+        end
         if key == 'h' then
-            self.body:applyForce(-self.speed, 0)
+            self.body:applyForce(-self.speed*mult, -self.jump*jmult)
         elseif key == 'l' then
-            self.body:applyForce(self.speed, 0)
+            self.body:applyForce(self.speed*mult, -self.jump*jmult)
         end
     end
 end
 
 function player:_pre_contact(shape, other, other_shape, coll)
     local nx, ny = coll:getNormal()
-    if ny > 0.85 and not self.coll_ground[other:name()] then -- approx. 45 deg
+    if ny >= 0.85 and not self.coll_ground[other:name()] then -- approx. 45 deg
         self.coll_ground[other:name()] = true
         self.ground = self.ground + 1
     end
